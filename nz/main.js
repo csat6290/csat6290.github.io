fed713e8b63c5bff27ca961fe62e549baa20917a
Luca <vintiluca@gmail.com>
2022-04-07 15:44:56 +0200

    lat, lng und zoom Variablen eingeführt

--- nz/main.js
+++ nz/main.js
@@ -1,12 +1,21 @@
 /* Neuseelandreise Skript */
 // einzeliger Kommentar
 
-var map = L.map('map').setView([-45.874167, 170.503611], 13);
+let lat = -45.874167;
+
+let lng = 170.503611;
+
+let zoom = 13;
+
+let map = L.map('map').setView([lat, lng], zoom);
+
+// let sind die Variablen
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([-45.874167, 170.503611]).addTo(map)
+L.marker([lat, lng]).addTo(map)
     .bindPopup('<h3>Dunedin</h3>')
     .openPopup();
+
