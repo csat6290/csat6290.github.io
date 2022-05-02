8df42aa60d7fab4c5bc0ac38e6c8ce531f173400
Luca <vintiluca@gmail.com>
2022-05-03 00:32:07 +0200

    kommentar

--- nz/main.js
+++ nz/main.js
@@ -119,3 +119,5 @@ let miniMap = new L.Control.MiniMap(
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     })).addTo(map);
+
+    //müsste jetzt klappen
