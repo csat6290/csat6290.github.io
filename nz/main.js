08bfbf1dd7cb874e44504a362012f57d9c6061f8
Luca <vintiluca@gmail.com>
2022-05-02 23:25:10 +0200

    minimap hinzugefügt

--- nz/main.js
+++ nz/main.js
@@ -98,3 +98,7 @@ L.control.fullscreen().addTo(map);
 L.control.scale({
     imperial: false,
 }).addTo(map)
+
+let miniMap = new L.Control.MiniMap(
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}
+     )).addTo(map);
